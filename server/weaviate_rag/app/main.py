a9505ca5c1baaa9566ab850b7e8b26f92ddca1d3
NH-Uday <nahinu95@gmail.com>
2025-07-10 23:29:21 +0200

    working python backend and React frontend

--- server/weaviate_rag/app/main.py
+++ server/weaviate_rag/app/main.py
@@ -1,9 +1,17 @@
-# weaviate_rag/app/main.py
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from app.services.rag import retrieve_answer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can limit this to ["http://localhost:3000"] in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatRequest(BaseModel):
     question: str
